5c19d17b66b56ed32bd3ed2e9d5a0d35c4890bc6
Yu-Hung Chen <83629846+eddychen86@users.noreply.github.com>
2022-02-23 01:15:39 +0800

    Update TestView.vue

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    // msg = message，不是固定名稱，是在 HelloWorld.vue 內所宣告的 message 的宣告名稱
     <HelloWorld msg="This is a TestView"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+// @ 是 /src 路徑簡寫
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
+  // name (待確認)
   name: 'TestView',
   components: {
     HelloWorld
